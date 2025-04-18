db4fd85fbff67712da3967566b4b70297c2318a6
unknown <jasimbdpro@gmail.com>
2025-04-18 23:28:16 +0600

    .

--- models/download.js
+++ models/download.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const downloadSchema = new mongoose.Schema({
     title: { type: String, required: true },
     url: { type: String, required: true },
+    cloudinaryId: { type: String, required: true },
 });
 
 const Download = mongoose.models.Download || mongoose.model('Download', downloadSchema);
